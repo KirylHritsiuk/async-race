63f72d313b6cad938d74c37e8f4d3910b5fb0bcc
KirylHritsiuk <pishime2017@gmail.com>
2022-07-28 21:56:47 +0300

    feat: add eslint

---
+++ async-race-client/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    'no-implicit-any': 'error'
+  },
+};
